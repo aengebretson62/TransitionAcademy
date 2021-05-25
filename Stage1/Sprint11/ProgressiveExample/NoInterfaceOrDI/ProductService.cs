0de337fcd61b7b120e4df9fc1468cf4b87c6ffe0
Alec Engebretson <alec.engebretson@doane.edu>
2021-05-25 09:15:05 -0500

    Fixing my mistake during my demo

--- Stage1/Sprint11/ProgressiveExample/NoInterfaceOrDI/ProductService.cs
+++ Stage1/Sprint11/ProgressiveExample/NoInterfaceOrDI/ProductService.cs
@@ -5,15 +5,14 @@ namespace NoInterfaceOrDI
     public class ProductService
     {
         private readonly FileLogger _fileLogger = new FileLogger();
-        private readonly DatabaseLogger _databaseLogger =
-          new DatabaseLogger();
+        private readonly DatabaseLogger _databaseLogger = new DatabaseLogger();
         public void LogToFile(string message)
         {
             _fileLogger.Log(message);
         }
         public void LogToDatabase(string message)
         {
-            _fileLogger.Log(message);
+            _databaseLogger.Log(message);
         }
     }
 }
