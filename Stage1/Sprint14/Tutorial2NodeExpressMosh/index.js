e3c44c09a38fc222b7d518226bc8fc675f7cf9fd
Alec Engebretson <alec.engebretson@doane.edu>
2021-06-01 13:39:19 -0500

    Committ and push after regenerating security token

--- Stage1/Sprint14/Tutorial2NodeExpressMosh/index.js
+++ Stage1/Sprint14/Tutorial2NodeExpressMosh/index.js
@@ -9,6 +9,8 @@ const courses = [
     { id: 1, name: 'Course 1'},
     { id: 2, name: 'Course 2'},
     { id: 3, name: 'Course 3'},
+    { id: 4, name: 'Course 4'},
+    { id: 5, name: 'Course 5'},
 ]
 
 app.get('/', (req,res) => {
