315cfcae7aa6c38193442fd180da2506e5358640
Alec Engebretson <alec.engebretson@doane.edu>
2022-05-10 16:25:45 -0500

    Class example working with inheritance

--- Stage1/ClassExample/Car.js
+++ Stage1/ClassExample/Car.js
@@ -1,18 +1,32 @@
-class Car 
+class Vehicle
   {
-    constructor(make, model, year) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
-    }
-    age() {
+      constructor(make,model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+      }
+   
+      info() {
+        return "My " + this.make + " " + this.model + " is " + this.age() + " years old.";
+      }
+
+     age() {
       // date = new Date();  // This will not work
       let date = new Date(); // This will work
       return date.getFullYear() - this.year;
     }
-    info() {
-        return "My " + this.make + " " + this.model + " is " + this.age() + " years old.";
-    }
+  }
+
+class Car extends Vehicle
+  {
+    constructor(type,make,model, year) {
+      super(make, model, year);
+      this.type = type;
+      }
+
+  show() {
+    return this.info() + ' It is a ' + this.type;
+   }
   }
 
   function myCarFunction() 
@@ -21,10 +35,11 @@ class Car
     var theMake = document.getElementById("fmake").value;
     var theModel = document.getElementById("fmodel").value;
     var theYear = document.getElementById("fyear").value;
+    var theType = document.getElementById("ftype").value;
 
     alert ("Creating the Car object and passing the values obtained...");
-    var newCar = new Car(theMake, theModel, theYear);
+    var newCar = new Car(theType,theMake, theModel, theYear);
 
     alert ("Providing the object information to the user");
-    document.getElementById("information").innerHTML = newCar.info();
+    document.getElementById("information").innerHTML = newCar.show();
   }
